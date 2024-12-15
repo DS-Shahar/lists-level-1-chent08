223793404fcc46ea85611adf0fca5a16a26f5653
Chen Tamir <chentamir4@gmail.com>
2024-12-15 12:20:37 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -229,6 +229,29 @@ public class Main {
 		}
 		return L1; // prints the new list
 	}
+	    public static Node<Integer> removeCommon2 ( Node<Integer> listA, Node<Integer> listB) {
+	    Node<Integer> current = new Node<>(0, listA);
+	    Node<Integer> head = current;
+	    Node<Integer> pB = listB;
+
+	    while (current.hasNext()) {
+	        int a = current.getNext().getValue();
+	        pB = listB;
+
+	        while (pB != null && !pB.getValue().equals(a)) {
+	            pB = pB.getNext();
+	        }
+
+	        if (pB != null) {
+	            current.setNext(current.getNext().getNext());
+	        } else {
+	            current = current.getNext();
+	        }
+	    }
+
+	    return head.getNext();
+	}
+
 	
 	
 	
